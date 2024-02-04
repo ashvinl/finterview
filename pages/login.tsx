ce6d2bc38a8cfe843e5544a8653cd24f451a061e
ashleykcheng <ashleykcheng@gmail.com>
2024-02-04 11:36:57 -0800

    login updates

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,5 +1,8 @@
 import Link from "next/link";
 import React from 'react';
+import "../styles/globals.css";
+import LogoImage from "../public/images/finterview-logo.png"
+import Image from 'next/image';
 
 // pages/index.js or pages/your-page.js
 
@@ -10,7 +13,7 @@ export default function LoginPage() {
         <div className="w-1/2 text-white p-10 justify-center" style={{ backgroundColor: "#1D402F" }}>
 
           <h1 className="text-4xl font-bold mb-4">Welcome to Finterview</h1>
-          <img src="/public/images/Finterview logo 300x300.png" alt="Finterview Logo" />
+          <Image src={LogoImage} alt="Finterview Logo" /> 
           <p className="mb-8">
             Finterview is an AI-powered video interview platform tailored towards aspiring finance professionals.
             Through progress tracking and instant feedback on first-rate simulated interviews, students get the
@@ -23,7 +26,7 @@ export default function LoginPage() {
   
         {/* Right side */}
         <div className="w-1/2 flex justify-center items-center bg-white">
-          <div className="w-full max-w-md">
+          <div className="w-3/5 max-w-md  my-10">
             <h2 className="text-2xl font-bold text-left mb-6">Sign in</h2>
             
               <form>
@@ -36,7 +39,7 @@ export default function LoginPage() {
                     placeholder="Enter Email or Username"
                   />
                 </div>
-                <div className="mb-4">
+                <div className="mb-4 text-right">
                   
                   <input
                     className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
@@ -44,9 +47,9 @@ export default function LoginPage() {
                     type="password"
                     placeholder="Password"
                   />
-                  <p className="text-right text-sm text-blue-500 hover:text-blue-800">
+                  <Link className=" text-sm hover:text-blue-800" href="/forgot">
                     Forgot Password?
-                  </p>
+                  </Link>
                 </div>
                 <div className="items-center justify-between">
                   <button
@@ -56,15 +59,11 @@ export default function LoginPage() {
                     Sign In
                   </button>
                 </div>
-                <div className="mt-4 flex items-center justify-center">
-                  <span className="text-gray-600 mr-2">Or Continue with</span>
-                  <button
-                    className="border rounded-full p-2 shadow"
-                    type="button"
-                  >
-                    G
-                  </button>
+                <div className="mt-4 flex items-center justify-center mb-4 py-4">
+                  <span className="text-gray-600 mr-2">Or Continue With</span>
+        
                 </div>
+               <GoogleButton />
               </form>
             
           </div>
@@ -73,3 +72,43 @@ export default function LoginPage() {
     );
   }
   
+  const GoogleButton = () => {
+    return (
+      <button
+        className="flex w-full justify-center gap-5 rounded bg-white py-2 px-4 text-sm font-bold drop-shadow-md hover:bg-gray-50"
+        
+      >
+        <GoogleLogo />
+       
+      </button>
+    );
+  };
+  
+  
+  const GoogleLogo = () => (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 775 794"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    
+    >
+      <path
+        d="M775 405.797C775 373.248 772.362 349.496 766.653 324.865H395.408V471.773H613.32C608.929 508.282 585.204 563.264 532.482 600.209L531.743 605.127L649.124 696.166L657.256 696.979C731.943 627.921 775 526.315 775 405.797"
+        fill="#4285F4"
+      />
+      <path
+        d="M395.408 792.866C502.167 792.866 591.792 757.676 657.256 696.979L532.482 600.209C499.093 623.521 454.279 639.796 395.408 639.796C290.845 639.796 202.099 570.741 170.463 475.294L165.826 475.688L43.772 570.256L42.1759 574.698C107.198 704.013 240.758 792.866 395.408 792.866Z"
+        fill="#34A853"
+      />
+      <path
+        d="M170.463 475.294C162.116 450.662 157.285 424.269 157.285 397C157.285 369.728 162.116 343.338 170.024 318.706L169.803 313.46L46.2193 217.373L42.1759 219.299C15.3772 272.961 0 333.222 0 397C0 460.778 15.3772 521.036 42.1759 574.698L170.463 475.294"
+        fill="#FBBC05"
+      />
+      <path
+        d="M395.408 154.201C469.656 154.201 519.74 186.31 548.298 213.143L659.891 104.059C591.356 40.2812 502.167 1.13428 395.408 1.13428C240.758 1.13428 107.198 89.9835 42.1759 219.299L170.024 318.706C202.099 223.259 290.845 154.201 395.408 154.201"
+        fill="#EB4335"
+      />
+    </svg>
+  );
